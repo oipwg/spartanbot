9da24c25c26ec5d8f6d6763a6d5eb6bdcfb70add
Sky Young <sky@alexandria.io>
2018-09-13 13:28:19 -0700

    Push up framework

---
+++ src/SpartanBot.js
@@ -0,0 +1,14 @@
+/**
+ * Rent hashrate based on a set of circumstances
+ */
+class SpartanBot {
+	/**
+	 * [constructor description]
+	 * @param  {[type]} options [description]
+	 * @return {[type]}         [description]
+	 */
+	constructor(options){
+
+	}
+	
+}
