af87f05046a5ad0543f593254975038257412700
orpheus <ryanachacon@gmail.com>
2018-09-17 17:05:54 -0700

    rental testsand functions working

--- src/RentalProviders/MRRProvider.js
+++ src/RentalProviders/MRRProvider.js
@@ -1,4 +1,5 @@
 import RentalProvider from './RentalProvider'
+import MiningRigRentals from 'miningrigrentals-api-v2'
 
 /**
  * A Rental Provider for MiningRigRentals
@@ -14,6 +15,8 @@ class MRRProvider extends RentalProvider {
 	 */
 	constructor(settings){
 		super(settings)
+
+		this.api = new MiningRigRentals({key: this.api_key, secret: this.api_secret})
 	}
 
 	/**
@@ -30,9 +33,158 @@ class MRRProvider extends RentalProvider {
 	 * @return {Promise} Returns a Promise that will resolve upon success, and reject on failure
 	 */
 	async testAuthorization(){
-		return true
+		try {
+			let profile = await this.api.whoami();
+			console.log(profile)
+			return !!(profile.success && profile.data && profile.data.authed);
+		} catch (err) {
+			throw new Error(err)
+		}
 	}
 
+	/**
+	 * Get MiningRigRentals Profile ID (needed to rent rigs)
+	 * @returns {Promise<number>} - the id of the first data object
+	 */
+	async getProfileID() {
+		let profile;
+		try {
+			profile = await this.api.getProfiles();
+		} catch (err) {
+			throw new Error(`error getting profile data: ${err}`)
+		}
+		if (profile.data) {
+			return profile.data[0].id
+		}
+	}
+	/**
+	 * Get the rigs needed to fulfill rental requirements
+	 * @param {number} hashrate - in megahertz(mh)
+	 * @param {number} duration - in hours
+	 * @returns {Promise<Array.<Object>>}
+	 */
+	async getRigsToRent(hashrate, duration) {
+		//get profileID
+		let profileID
+		try {
+			profileID =  await this.getProfileID()
+		} catch (err) {
+			throw new Error(`Could not fetch profile ID \n ${err}`)
+		}
+
+		let rigOpts = {
+			type: 'scrypt',
+			minhours: {
+				max: duration
+			}
+		}
+		let rigsRequest;
+		try {
+			rigsRequest = await this.api.getRigs(rigOpts)
+		} catch (err) {
+			throw new Error(`Could not fetch rig list \n ${err}`)
+		}
+		let available_rigs = [];
+		if (rigsRequest.success && rigsRequest.data) {
+			if (rigsRequest.data.records.length === 0) {
+				throw new Error(`No rigs found`)
+			}
+			let newRPIrigs = [], allOtherRigs = [];
+			for (let rig of rigsRequest.data.records) {
+				if (rig.rpi === 'new') {
+					newRPIrigs.push(rig)
+				} else {allOtherRigs.push(rig)}
+			}
+			allOtherRigs.sort((a,b) => {
+				return (b.rpi - a.rpi)
+			});
+			available_rigs = newRPIrigs.concat(allOtherRigs)
+		}
+
+		let rigs_to_rent = [], hashpower = 0;
+		for (let rig of available_rigs) {
+			if ((hashpower + rig.hashrate.advertised.hash) <= hashrate) {
+				let tmpObj = {};
+				tmpObj['rig'] = Number(rig.id);
+				tmpObj['length'] = duration
+				tmpObj['profile'] = Number(profileID)
+				// tmpObj['hashrate'] = rig.hashrate.advertised.hash
+				hashpower += rig.hashrate.advertised.hash
+				rigs_to_rent.push(tmpObj)
+			}
+		}
+		return rigs_to_rent
+	}
+
+	/**
+	 * Rent rigs based on hashrate and time
+	 * @param {Object} options
+	 * @param {number} options.hashrate - The hashrate in MH
+	 * @param {number} options.duration - Duration of rent
+	 * @param {Function} [options.confirm] - An async function for confirmation
+	 * @param {string} [options.type='scrypt'] - Type of rig (Scrypt, x11, sha256, etc)
+	 * @returns {Promise<*>}
+	 */
+	async rent(options) {
+		//get rigs
+		let rigs_to_rent = [];
+		try {
+			rigs_to_rent = await this.getRigsToRent(options.hashrate, options.duration)
+		} catch (err) {
+			throw new Error(`Failed to fetch rigs to rent \n ${err}`)
+		}
+
+		//confirmation
+		if (options.confirm) {
+			try {
+				let confirmed = await options.confirm(rigs_to_rent)
+				if (!confirmed) {
+					return {
+						success: false,
+						info: "Rental Cancelled"
+					}
+				}
+			} catch (err) {
+				throw new Error(err)
+			}
+		}
+
+		//rent rigs
+		let rentalConfirmation = {};
+		let time = Date.now()
+		for (let rig of rigs_to_rent) {
+			try {
+				let rental = await this.api.createRental(rig)
+				let newTime = Date.now();
+				console.log((newTime-time)/1000)
+				time = newTime
+				rentalConfirmation[rig.rig] = rental
+				console.log(rental)
+			} catch (err) {
+				rentalConfirmation[rig.rig] = `Error renting rig: ${err}`
+			}
+		}
+		// let rentalObject = {}
+		// for (let rig of rigs_to_rent) {
+		// 	rentalObject[`${rig.rig}`] = rig
+		// }
+		// console.log('rental object: ', rentalObject)
+		// let rentalPromises = {}
+		// for (let rig in rentalObject) {
+		// 	rentalPromises[rig] = this.api.createRental(rentalObject[rig])
+		// }
+		//
+		// let rentalConfirmation = {};
+		// for (let rig in rentalPromises) {
+		// 	try {
+		// 		rentalConfirmation[rig] = await rentalPromises[rig]
+		// 	} catch (err) {
+		// 		rentalConfirmation[rig] = `Error attempting to rent ${rentalObject[rig]} \n ${err}`
+		// 	}
+		// }
+		//return confirmation
+		return rentalConfirmation
+	}
 	/**
 	 * Get back a "Serialized" state of the Provider
 	 * @return {Object} Returns a JSON object that contains the current rental provider state
---
+++ tests/MRRProvider.test.js
@@ -0,0 +1,71 @@
+import MRRProvider from '../src/RentalProviders/MRRProvider';
+import apikey from './apikey';
+
+describe("MRRProvider", () => {
+	it('should authorize MRR API access | testAuthorization', async () => {
+		let mrr = new MRRProvider(apikey);
+		let thrown;
+		try {
+			let success = await mrr.testAuthorization();
+			expect(success).toBeTruthy()
+		} catch (err) {
+			thrown = false;
+			expect(thrown).toBeFalsy()
+		}
+	});
+	it('should get my profile ID | getProfileID', async () => {
+		let mrr = new MRRProvider(apikey);
+		let thrown;
+		try {
+			let profileID = await mrr.getProfileID();
+			console.log(profileID)
+			expect(typeof profileID === 'number').toBeTruthy()
+		} catch (err) {
+			thrown = false;
+			expect(thrown).toBeFalsy()
+		}
+	});
+	it('should fetch qualified rigs| getRigsToRent', async () =>{
+		let mrr = new MRRProvider(apikey);
+		let thrown;
+		let hashMh = 5000, duration = 5;
+		try {
+			let rigs = await mrr.getRigsToRent(hashMh, duration);
+			// expect(success).toBeTruthy()
+			let hashpower = 0;
+			for (let rig of rigs) {
+				hashpower += rig.hashrate
+			}
+			let enoughHash= false
+			if (hashpower <= 5000) {
+				enoughHash = true
+			}
+			expect(enoughHash).toBeTruthy()
+		} catch (err) {
+			thrown = false;
+			expect(thrown).toBeFalsy()
+		}
+	});
+	it('rent rigs', async () => {
+		let mrr = new MRRProvider(apikey);
+		let rentOptions = {
+			hashrate: 5000,
+			duration: 5,
+			confirm: confirmFn
+		}
+		try {
+			let rentalConfirmation = await mrr.rent(rentOptions);
+			console.log('rental confirmation: ', rentalConfirmation)
+		} catch (err) {
+			console.log(`Error: ${err}`)
+			expect(err).toBeUndefined()
+		}
+	}, 250 * 1000);
+})
+
+let confirmFn = async (data) => {
+	return true
+	// setTimeout( () => {
+	// 	Promise.resolve(true)
+	// }, 2000)
+}
