958f2d47573c4f0945ffcdc929e6326e57267e71
orpheus <ryanachacon@gmail.com>
2018-10-09 11:57:16 -0700

    add test for NH manual rent

--- tests/NiceHashProvider.test.js
+++ tests/NiceHashProvider.test.js
@@ -3,8 +3,8 @@ import { config } from 'dotenv'
 config()
 
 const apikey = {
-	key: process.env.NICEHASH_API_KEY,
-	id: process.env.NICEHASH_API_ID
+	api_key: process.env.NICEHASH_API_KEY,
+	api_id: process.env.NICEHASH_API_ID
 };
 
 describe('NiceHashProvider', () => {
@@ -18,4 +18,29 @@ describe('NiceHashProvider', () => {
 			expect(typeof await api._getBalance() === 'number')
 		})
 	});
+	describe('Rent', () => {
+		it('Manual Rent', async () => {
+			let nh = new NiceHashProvider(apikey);
+
+			let poolOptions = {
+				algo: 'scrypt',
+				host: 'snowflake.oip.fun',
+				port: 3043,
+				user: 'FAkFS9JonHkuZBhV9bbwXnsNRBwWSR5ve6',
+				pass: 'x',
+				location: 1,
+				name: 'Ryans NH'
+			}
+			await nh.createPool(poolOptions)
+
+			let rentOptions = {
+				amount: 0.005,
+				limit: .01,
+				price: .500
+			}
+
+			console.log(await nh.manualRent(rentOptions))
+
+		})
+	})
 })
