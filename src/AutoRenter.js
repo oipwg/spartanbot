9fdc4fec548af95965d625dc788ada5fc509cc09
orpheus <ryanachacon@gmail.com>
2018-10-10 22:13:29 -0700

    more systems notes

--- src/AutoRenter.js
+++ src/AutoRenter.js
@@ -199,36 +199,52 @@ class AutoRenter {
 	//how does each provider talk to eachother?
 	//if I ask for a small amount of hash, what happens.
 	// ----individual preprocessing. Each provider preprocesses itself with the given information----- make this a RentalProvider parent method
+	//each preprocess only needs hash and duration
+
 	//get balance
 	//they each check to see if they can fulfill that request and the best price is chosen
 	//if they can't fulfill it, they'll tell you how much they can
 	//each returns an object with the needed details of what they can do along with a copy/link to themselves to be acted upon if needed
+
+	//returned preprocess should be: {provider, balance, price, limit, duration, amount, [rigs_to_rent], {preprocessMRR}}
+	//for mrr providers this can be done with one function for all of them due to the nature of possibly renting the same rigs
+	//for nicehash, orders don't happen via rig, so each provider can get processed individually and return the same object)
+
+	//the preprocess function will check
+		//MRR
+			//collect all mrr providers ***MRR takes many providers acting as one
+			//use one of them to find rigs to rent based on hashrate and duration
+			//split up work evenly and their preprocess information
+			//this is where the providers get their own badge
+				//this part needs to happen. Providers cannot rent for each other!!!
+			//if rigs to rent === 0, status => Error
+			//returns array of objects that contain a provider, the rigs they can rent, limit, amount (cost), price, status
+
+		//NH Preprocess *** Each Nice Hash provider is its own thing
+			//Check balance against .005
+			//check limit against hashrate given
+			//calculate price
+
+			//if price is < .5, then set price to .5 and calculate amount
+			//if amount is greater than balance, status => LOW_BALANCE
+			//set amount to max balance and calculate new limit based on price, amount, and duration
+			//return limit of what it can rent with available balance
+			//return object of that contains a provider, its balance, limit, price, amount, status
+
 	// ---- end of individual preprocess
+
 	//the actual preprocess pushes these returned objects into an array it will analyze
 	// figure out this part... return the providers it will be renting from
+			//if one success, rent
+			//if two or more success, rent one with best price
+			//if no success and all low balances
+				//add them up to get close to desire limit
 	//confirmation function
 	//for each provider picked, use its link to rent with the information it gave us back from the preprocess
 
-	//returned preprocess should be: {provider, balance, price, limit, duration, amount, [rigs_to_rent], {preprocessMRR}}
-			//for mrr providers this can be done with one function for all of them due to the nature of possibly renting the same rigs
-			//for nicehash, orders don't happen via rig, so each provider can get processed individually and return the same object)
-		//the preprocess function will check
-			//MRR
-				//that the limit returned will equal limit given
-
-			//NH Preprocess
-				//Check balance against .005
-				//check limit against hashrate given
-				//calculate price
-
-				//if price is < .5, then set price to .5 and calculate amount
-				//if amount is greater than balance, status => LOW_BALANCE
-				//set amount to max balance and calculate new limit based on price, amount, and duration
-				//else write new amount (cost)
-
 	//STATUS MESSAGES
-		//ERROR - cannot rent
-		//LOW_BALANCE - can rent partially
+		//ERROR - cannot rent. either insufficient funds or http error
+		//LOW_BALANCE - rent as much as you can with available balance
 		//NORMAL - can rent all required
 
 
