24cd1d5c2bfa050473aecc1bc4db2a1c706832b7
orpheus <ryanachacon@gmail.com>
2018-10-11 13:20:17 -0700

    remove old return object

--- src/RentalProviders/NiceHashProvider.js
+++ src/RentalProviders/NiceHashProvider.js
@@ -182,7 +182,6 @@ class NiceHashProvider extends RentalProvider {
 
 		if (balance < 0.005 || hashrate/1000/1000 < 0.01) {
 			status.status = ERROR
-			return {success: false, message: `Balance must be >= 0.005 and hashrate must be >= 0.01 TH`, status}
 			let message;
 			if (balance < 0.005)
 				message = `Balance must be >= 0.005`
