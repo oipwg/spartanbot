7eee988b0c0c0148f2b0bc336dd4d6ecc2150e05
orpheus <ryanachacon@gmail.com>
2018-10-18 18:13:40 -0700

    create constants file for exports

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const TriggerRental = 'TriggerRental'
+export const RentalFunctionFinish = 'RentalFunctionFinish'
+
+export const RECEIPT = 'RECEIPT'
+export const NORMAL = 'NORMAL'
+export const WARNING = 'WARNING'
+export const CUTOFF = 'CUTOFF'
+export const EXTENSION = 'EXTENSION'
+export const ERROR = 'ERROR'
+export const LOW_BALANCE = 'LOW_BALANCE'
+export const LOW_LIMIT = 'LOW_LIMIT'
+export const LOW_HASHRATE = 'LOW_HASHRATE'
+export const RENTAL_SUCCESS = 'RENTAL_SUCCESS'
+export const RENTAL_WARNING = 'RENTAL_WARNING'
+export const RENTAL_ERROR = 'RENTAL_ERROR'
+
+export const ManualRent = 'ManualRent'
+export const SpotRent = 'SpotRent'
+export const TradeBot = 'TradeBot'
+export const SpartanSense = 'SpartanSense'
+export const CollectiveDefense = 'CollectiveDefense'
+
+
