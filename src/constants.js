56dca8244756feb42d56a03f058b01a304371e54
orpheus <ryanachacon@gmail.com>
2018-10-23 14:37:51 -0700

    organized constants

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,13 @@
+//Triggers
 export const TriggerRental = 'TriggerRental'
 export const RentalFunctionFinish = 'RentalFunctionFinish'
+export const StartupSpotRentalStrategy = 'StartupSpotRentalStrategy'
+export const StartupChainScanner = 'StartupChainScanner'
+export const CHECK_SPOT_PROFIT = 'CHECK_SPOT_PROFIT'
+export const CHECK_NODE_STATUS = 'CHECK_NODE_STATUS'
+export const STARTUP = 'STARTUP'
 
+//Status and Status Types
 export const RECEIPT = 'RECEIPT'
 export const NORMAL = 'NORMAL'
 export const WARNING = 'WARNING'
@@ -14,16 +21,16 @@ export const RENTAL_SUCCESS = 'RENTAL_SUCCESS'
 export const RENTAL_WARNING = 'RENTAL_WARNING'
 export const RENTAL_ERROR = 'RENTAL_ERROR'
 
+//Rental Strategie Constants
 export const ManualRent = 'ManualRent'
-export const SpotRent = 'SpotRent'
+export const SpotRental = 'SpotRental'
 export const TradeBot = 'TradeBot'
 export const SpartanSense = 'SpartanSense'
 export const CollectiveDefense = 'CollectiveDefense'
+export const GENERIC = 'GENERIC'
 
-export const error = 'error'
-export const StartupSpotRentalStrategy = 'StartupSpotRentalStrategy'
-export const StartupChainScanner = 'StartupChainScanner'
+
+//Alerts
 export const NODE_SYNCED = 'NODE_SYNCED'
-export const CHECK_SPOT_PROFIT = 'CHECK_SPOT_PROFIT'
-export const CHECK_NODE_STATUS = 'CHECK_NODE_STATUS'
-export const STARTUP = 'STARTUP'
+export const error = 'error'
+
