70a9e7eb7560775f9278fd544845c8cdea2407de
orpheus <ryanachacon@gmail.com>
2018-10-23 14:36:27 -0700

    updated NiceHashProvider tests

--- tests/NiceHashProvider.test.js
+++ tests/NiceHashProvider.test.js
@@ -22,32 +22,43 @@ describe('NiceHashProvider', () => {
 		it('preprocess nicehash rent', async () => {
 			let nh = new NiceHashProvider(apikey);
 
-			let hashrate = 800000
+			let hashrate = 80000
 			let duration = 3
 
-			let preprocess = await nh.manualRentPreprocess(hashrate, duration)
-			expect(preprocess.price).toBeDefined()
+			let preprocess = await nh.preprocessRent(hashrate, duration)
+			console.log(preprocess)
+			expect(preprocess.market).toEqual("NiceHash")
+			expect(preprocess.status).toBeDefined()
+			expect(preprocess.limit).toEqual(0.08)
+			expect(preprocess.price).not.toBeNaN()
+			expect(preprocess.amount).not.toBeNaN()
+			expect(preprocess.duration).not.toBeNaN()
+			expect(preprocess.balance).not.toBeNaN()
 		});
 		it.skip('Manual Rent', async () => {
 			let nh = new NiceHashProvider(apikey);
 
 			let poolOptions = {
 				algo: 'scrypt',
-				host: 'snowflake.oip.fun',
-				port: 3043,
-				user: 'FAkFS9JonHkuZBhV9bbwXnsNRBwWSR5ve6',
+				host: 'thecoin.pw',
+				port: 3978,
+				user: 'orpheus.1',
 				pass: 'x',
 				location: 1,
-				name: 'Ryans NH'
+				name: 'Orpheus'
 			}
 			await nh.createPool(poolOptions)
 
 			let rentOptions = {
+				market: "NiceHash",
 				amount: 0.005,
 				limit: .01,
 				price: .500
 			}
-			// console.log(await nh.rent(rentOptions))
+
+			let nhOrder = await nh.rent(rentOptions)
+			expect(nhOrder.success).toBeTruthy()
+			// console.log('nhOrder: ', nhOrder)
 		})
 	})
 })
